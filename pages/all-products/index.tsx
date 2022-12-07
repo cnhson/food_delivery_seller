2fc0102195676fe6a4fb14af677288fdb5ae976c
bao_cryptoviet <baonguyen120301@gmail.com>
2022-12-07 16:17:40 +0700

    add modal for edit product

--- pages/all-products/index.tsx
+++ pages/all-products/index.tsx
@@ -1,136 +1,355 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   createStyles,
   Table,
   Checkbox,
-  ScrollArea,
+  useMantineTheme,
   Group,
-  Avatar,
+  Modal,
   Text,
+  Paper,
+  Button,
+  FileInput,
+  TextInput,
+  Textarea,
+  Select,
+  NumberInput,
+  Image,
 } from "@mantine/core";
+import { IconEdit } from "@tabler/icons";
+import axios from "axios";
+//import Image from "next/image";
 
 const useStyles = createStyles((theme) => ({
+  root: {
+    marginTop: 40,
+    backgroundColor: "#EDF2FF",
+    height: "100%",
+    width: "75vw",
+  },
   rowSelected: {
     backgroundColor:
       theme.colorScheme === "dark"
         ? theme.fn.rgba(theme.colors[theme.primaryColor][7], 0.2)
         : theme.colors[theme.primaryColor][0],
   },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  thead: {
+    background: "#D1D1D1",
+  },
+  textInput: {
+    width: 400,
+  },
 }));
 
-interface TableSelectionProps {
-  data: {
-    avatar: string;
-    name: string;
-    email: string;
-    job: string;
-    id: string;
-  }[];
+interface TableProducts {
+  product_id: string;
+  name: string;
+  price: string;
+  type: string;
 }
 
+type TypeProduct = {
+  value: string;
+  label: string;
+};
+
 export default function TableSelection() {
-  const data = [
-    {
-      id: "1",
-      avatar:
-        "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-      name: "Robert Wolfkisser",
-      job: "Engineer",
-      email: "rob_wolf@gmail.com",
-    },
-    {
-      id: "2",
-      avatar:
-        "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-      name: "Jill Jailbreaker",
-      job: "Engineer",
-      email: "jj@breaker.com",
-    },
-    {
-      id: "3",
-      avatar:
-        "https://images.unsplash.com/photo-1632922267756-9b71242b1592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-      name: "Henry Silkeater",
-      job: "Designer",
-      email: "henry@silkeater.io",
-    },
-    {
-      id: "4",
-      avatar:
-        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-      name: "Bill Horsefighter",
-      job: "Designer",
-      email: "bhorsefighter@gmail.com",
-    },
-    {
-      id: "5",
-      avatar:
-        "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-      name: "Jeremy Footviewer",
-      job: "Manager",
-      email: "jeremy@foot.dev",
-    },
-  ];
-  const { classes, cx } = useStyles();
-  const [selection, setSelection] = useState(["1"]);
-  const toggleRow = (id: string) =>
-    setSelection((current) =>
-      current.includes(id)
-        ? current.filter((item) => item !== id)
-        : [...current, id]
-    );
-  const toggleAll = () =>
-    setSelection((current) =>
-      current.length === data.length ? [] : data.map((item) => item.id)
+  const [productArray, setProductArray] = useState<Array<TableProducts>>([]);
+  const { classes } = useStyles();
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  const store_id = sessionStorage.getItem("Store");
+
+  const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<any>();
+  const [typeChosen, setTypeChosen] = useState<string>("");
+
+  // product info
+  const [nameProduct, setNameProduct] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+
+  // check if empty
+  const [emptyName, setEmptyName] = useState<boolean>(false);
+  const [emptyDescription, setEmptyDescription] = useState<boolean>(false);
+  const [emptyPrice, setEmptyPrice] = useState<boolean>(false);
+  const [emptyType, setEmptyType] = useState<boolean>(false);
+
+  // type of product
+  const [typeProduct, setTypeProduct] = useState<Array<TypeProduct>>([]);
+
+  // loading
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function getAllProducts() {
+    try {
+      const response = await axios.get(
+        process.env.API + "menu/get-all-products/" + store_id
+      );
+
+      if (response.data.length > 0) {
+        const dataArray: Array<TableProducts> = [];
+        for (let i = 0; i < response.data.length; i++) {
+          const { id, name, price, type } = response.data[i];
+          const data: TableProducts = {
+            product_id: id,
+            name: name,
+            price: price,
+            type: type,
+          };
+
+          dataArray.push(data);
+        }
+
+        setProductArray(dataArray);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // get all type of product
+  async function getAllProductType() {
+    const response = await axios.get(
+      process.env.API + "menu/get-all-product-type/" + store_id
     );
 
-  const rows = data.map((item) => {
-    const selected = selection.includes(item.id);
+    const types = response.data;
+    let typeArray: Array<TypeProduct> = [];
+    for (let i = 0; i < types.length; i++) {
+      let data: TypeProduct = {
+        value: "",
+        label: "",
+      };
+      data.value = types[i].id;
+      data.label = types[i].name;
+
+      typeArray.push(data);
+    }
+
+    setTypeProduct(typeArray);
+  }
+
+  async function getProductInfo(product_id: string) {
+    try {
+      const response = await axios.get(
+        process.env.API + "menu/get-product/" + product_id
+      );
+
+      const { name, type_id, description, image, price } = response.data[0];
+      setNameProduct(name);
+      setDescription(description);
+      setPrice(price);
+      setTypeChosen(type_id);
+      setFileUrl(process.env.IPFS_URL + image);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getAllProducts();
+    getAllProductType();
+  }, []);
+
+  const rows = productArray.map((item) => {
+    const Icon = IconEdit;
     return (
-      <tr key={item.id} className={cx({ [classes.rowSelected]: selected })}>
-        <td>
-          <Checkbox
-            checked={selection.includes(item.id)}
-            onChange={() => toggleRow(item.id)}
-            transitionDuration={0}
-          />
-        </td>
+      <tr key={item.product_id}>
         <td>
           <Group spacing="sm">
-            <Avatar size={26} src={item.avatar} radius={26} />
             <Text size="sm" weight={500}>
-              {item.name}
+              {item.product_id}
             </Text>
           </Group>
         </td>
-        <td>{item.email}</td>
-        <td>{item.job}</td>
+        <td>{item.name}</td>
+        <td>$ {item.price}</td>
+        <td>{item.type}</td>
+        <td>
+          <Button
+            variant="default"
+            onClick={() => {
+              setOpened(true);
+              getProductInfo(item.product_id);
+            }}
+          >
+            <Icon size={22} stroke={1.5} />
+          </Button>
+        </td>
       </tr>
     );
   });
 
   return (
-    <ScrollArea>
-      <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
-        <thead>
-          <tr>
-            <th style={{ width: 40 }}>
-              <Checkbox
-                onChange={toggleAll}
-                checked={selection.length === data.length}
-                indeterminate={
-                  selection.length > 0 && selection.length !== data.length
-                }
-                transitionDuration={0}
-              />
-            </th>
-            <th>User</th>
-            <th>Email</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
-    </ScrollArea>
+    <div className={classes.root}>
+      <Group position="center">
+        <Paper withBorder p="xl" radius="md">
+          <Text
+            className={classes.title}
+            component="span"
+            align="center"
+            variant="gradient"
+            gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+            size="xl"
+            weight={700}
+            style={{ fontFamily: "Greycliff CF, sans-serif" }}
+          >
+            ALL PRODUCTS
+          </Text>
+          <Table withBorder sx={{ minWidth: 800 }} verticalSpacing="sm">
+            <thead className={classes.thead}>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Group</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </Table>
+          <Modal
+            overlayColor={
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[9]
+                : theme.colors.gray[2]
+            }
+            overlayOpacity={0.55}
+            overlayBlur={3}
+            opened={opened}
+            size="auto"
+            onClose={() => setOpened(false)}
+          >
+            <Paper>
+              <Text
+                className={classes.title}
+                component="span"
+                align="center"
+                variant="gradient"
+                gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+                size="xl"
+                weight={700}
+                style={{ fontFamily: "Greycliff CF, sans-serif" }}
+              >
+                EDIT PRODUCT
+              </Text>
+              <Group position="apart" mb={50}>
+                <Paper>
+                  <FileInput
+                    label="Your product's image"
+                    placeholder="Pick an image of your product"
+                    required
+                    mt="md"
+                    value={file}
+                    onChange={(value) => {
+                      if (value) {
+                        setFileUrl(URL.createObjectURL(value));
+                        setFile(value);
+                      }
+                    }}
+                  />
+                  <Group mt="md">
+                    <Image src={fileUrl} height={400} width={400} alt={""} />
+                  </Group>
+                </Paper>
+                <Paper ml={40}>
+                  <TextInput
+                    placeholder="What is your product's name?"
+                    label="Name of the product"
+                    radius="md"
+                    withAsterisk
+                    value={nameProduct}
+                    className={classes.textInput}
+                    onChange={(value) =>
+                      setNameProduct(value.currentTarget.value)
+                    }
+                    error={emptyName}
+                  ></TextInput>
+                  {emptyName ? (
+                    <Text fz="xs" c="red">
+                      Name is required
+                    </Text>
+                  ) : (
+                    <></>
+                  )}
+                  <Textarea
+                    placeholder="Description of your product"
+                    label="Description"
+                    radius="md"
+                    mt="md"
+                    withAsterisk
+                    value={description}
+                    onChange={(value) =>
+                      setDescription(value.currentTarget.value)
+                    }
+                    error={emptyDescription}
+                  ></Textarea>
+                  {emptyDescription ? (
+                    <Text fz="xs" c="red">
+                      Description is required
+                    </Text>
+                  ) : (
+                    <></>
+                  )}
+                  <Select
+                    label="Your product type"
+                    placeholder="Pick one"
+                    required
+                    searchable
+                    nothingFound="No options"
+                    mt="md"
+                    error={emptyType}
+                    data={typeProduct}
+                    value={typeChosen}
+                    onChange={(value) => setTypeChosen(value!)}
+                  />
+                  {emptyType ? (
+                    <Text fz="xs" c="red">
+                      Product Type is required
+                    </Text>
+                  ) : (
+                    <></>
+                  )}
+                  <NumberInput
+                    label="Price"
+                    radius="md"
+                    mt="md"
+                    withAsterisk
+                    defaultValue={price}
+                    precision={2}
+                    min={0}
+                    parser={(value) => value!.replace(/\$\s?|(,*)/g, "")}
+                    onChange={(value) => setPrice(value!)}
+                    formatter={(value) =>
+                      !Number.isNaN(parseFloat(value!))
+                        ? `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                        : "$ "
+                    }
+                    error={emptyPrice}
+                  ></NumberInput>
+                  {emptyPrice ? (
+                    <Text fz="xs" c="red">
+                      Price must higher than 0
+                    </Text>
+                  ) : (
+                    <></>
+                  )}
+                </Paper>
+              </Group>
+
+              <Button fullWidth loading={loading}>
+                Edit Product
+              </Button>
+            </Paper>
+          </Modal>
+        </Paper>
+      </Group>
+    </div>
   );
 }
